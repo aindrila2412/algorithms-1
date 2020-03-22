073fe095e720c5db423dce28ed8370bfd83a90d8
neerajp99 <neerajp1999@gmail.com>
2020-03-22 12:30:46 +0530

    Initial commit

---
+++ strassen_matrix_multiplication.py
@@ -0,0 +1,30 @@
+# Implementation of Strassen Subcubic Matrix Multiplication 
+
+"""
+    Naive Approach
+
+    ---------
+    Parameters
+    ---------
+    N number of matrices
+
+    ---------
+    Returns
+    ---------
+    Multiplied matrix 
+
+    ---------
+    Time Complexity
+    ---------
+    O(n^3)
+
+    ---------
+    Test Cases
+    ---------
+    [1, 20, 6, 4, 5]
+    => ([1, 4, 5, 6, 20], 5)
+
+"""
+
+def naiveStrassen():
+	
