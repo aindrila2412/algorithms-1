f4804cac10822857a61a0160229579d454f687e1
neerajp99 <neerajp1999@gmail.com>
2020-07-01 23:02:46 +0530

    Update strassen

--- strassen_matrix_multiplication.py
+++ strassen_matrix_multiplication.py
@@ -1,4 +1,4 @@
-# Implementation of Strassen Subcubic Matrix Multiplication 
+# Implementation of Strassen Subcubic Matrix Multiplication
 
 """
     Naive Approach
@@ -11,7 +11,7 @@
     ---------
     Returns
     ---------
-    Multiplied matrix 
+    Multiplied matrix
 
     ---------
     Time Complexity
@@ -25,6 +25,42 @@
     => ([1, 4, 5, 6, 20], 5)
 
 """
+def naiveStrassen(a, b, n):
+    c = [[0, 0], [0, 0]]
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                c[i][j] += a[i][k] * b[k][j]
+    return c
 
-def naiveStrassen():
-	
+a = [[1, 2], [3, 4]]
+b = [[5, 6], [0, 7]]
+n = 2
+
+print(naiveStrassen(a, b, n))
+
+"""
+    Divide and Conquer Approach
+
+    ---------
+    Parameters
+    ---------
+    N number of matrices
+
+    ---------
+    Returns
+    ---------
+    Multiplied matrix
+
+    ---------
+    Time Complexity
+    ---------
+    O(n^3)
+
+    ---------
+    Test Cases
+    ---------
+    [1, 20, 6, 4, 5]
+    => ([1, 4, 5, 6, 20], 5)
+
+"""
