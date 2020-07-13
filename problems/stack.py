64028983c2ff8b8c459373086f7a04c772037d37
neerajp99 <neerajp1999@gmail.com>
2020-07-13 20:25:11 +0530

    Add stack

---
+++ problems/stack.py
@@ -0,0 +1,29 @@
+# Implementation of stack data structure
+
+class Stack():
+	def __init__(self):
+		self.items = []
+
+	def push(self, item):
+		self.items.append(item)
+
+	def pop(self):
+		return self.items.pop()
+
+	def is_empty(self):
+		return self.items == []
+
+	def peek(self):
+		if not self.is_empty():
+			return self.items[-1]
+
+	def get_items(self):
+		return self.items
+
+stack = Stack()
+stack.push('A')
+stack.push('B')
+print(stack.get_items())
+print(stack.peek())
+
+
