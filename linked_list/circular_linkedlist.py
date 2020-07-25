33b3ab8485eff6739c3f659a03643891aedf2bc0
neerajp99 <neerajp1999@gmail.com>
2020-07-26 05:29:31 +0530

    Add josephus method

--- linked_list/circular_linkedlist.py
+++ linked_list/circular_linkedlist.py
@@ -117,6 +117,40 @@ class Circular_Linked_List:
 		print('\n')
 		new_list.print_list()
 
+	# Josephus Problem 
+	def remove_josephus_node(self, node):
+		if self.head:
+			# If the node is the head node
+			if self.head == node:
+				current = self.head 
+				while current.next != self.head:
+					current = current.next 
+				if self.head == self.head.next:
+					self.head = None 
+				else:
+					current.next = self.head.next 
+					self.head = self.head.next
+			else:
+				current = self.head 
+				previous = None 
+				while current.next != self.head:
+					previous = current 
+					current = current.next 
+					if current == node:
+						previous.next = current.next 
+						current = current.next
+
+	def josephus_circle(self, step):
+		current = self.head 
+
+		while self.circular_length() > 1:
+			count = 1 
+			while count != step:
+				current = current.next 
+				count += 1
+			print('KILL: ' + str(current.data))
+			self.remove_josephus_node(current)
+			current = current.next
 
 
 cllist = Circular_Linked_List()
@@ -124,8 +158,10 @@ cllist.append("C")
 cllist.append("D")
 cllist.prepend("B")
 cllist.prepend("A")
-# cllist.print_list()
+cllist.print_list()
 # cllist.remove_nodes("A")
 # cllist.print_list()
 # print(cllist.circular_length())
-cllist.split_list()
+# cllist.split_list()
+cllist.josephus_circle(2)
+cllist.print_list()
