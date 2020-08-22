3ae5359b6a211ac8684c8371109a90d54337f8b6
neerajp99 <neerajp1999@gmail.com>
2020-08-22 07:03:46 +0530

    Add quadratic time method

--- arrays/max_subarray.py
+++ arrays/max_subarray.py
@@ -1,22 +1,37 @@
 """
-Finding the maximum sub arrays inside a list
+	
 """
 
+# Method 1 (Naive)
+# Time complexity: O(n^3)
 def maximum_subarray(nums):
 	# Arbitary maximum sum
 	maximum_sum = -1000000
-	final = []
 
 	for i in range(0, len(nums)):
 		for j in range(i, len(nums)):
 			temp_sum = 0
-			temp_arr = []
 
 			for k in range(i, j + 1):
 				temp_sum += nums[k]
-				maximum_sum = max(maximum_sum, temp_sum)
-				temp_arr.append(k)
-			final.append(temp_arr)
+			maximum_sum = max(maximum_sum, temp_sum)
+
+	return maximum_sum
+
+
+# Method 2
+# Time complexity: O(n^2)
+def maximum_subarray(nums):
+	# Arbitary maximum sum 
+	maximum_sum = -100000
+
+	for i in range(0, len(nums)):
+		temp_sum = 0
+		for j in range(i, len(nums)):
+			temp_sum += nums[j]
+			maximum_sum = max(maximum_sum, temp_sum)
 	return final
+
+
 
 print(maximum_subarray([-2,1,-3,4,-1,2,1,-5,4]))
