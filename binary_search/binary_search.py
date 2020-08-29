36953b14be60fa901a029300a521101553431ce7
neerajp99 <neerajp1999@gmail.com>
2020-08-30 02:30:43 +0530

    Add recursive approach for binary search

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -7,7 +7,7 @@ Assumes array as sorted in ascending order.
 	- Recursive approach
 """
 
-# Iterative appraoch
+# Iterative approach
 def iterative_binary_search(arr, target):
 	low = 0
 	high = len(arr) - 1
@@ -22,6 +22,20 @@ def iterative_binary_search(arr, target):
 			low = mid + 1
 	return False
 
+# Recursive approach
+def recursive_binary_search(arr, target, low, high):
+	if low > high:
+		return False
+	else:
+		mid = (low + high) // 2
+		if target == arr[mid]:
+			return True
+		elif target < mid:
+			return recursive_binary_search(arr, target, low, mid -1)
+		else:
+			return recursive_binary_search(arr, target, mid + 1, high)
+
 print(iterative_binary_search([-1,2,3,4,5,6,7,9], -1))
+print(recursive_binary_search([-1,2,3,4,5,6,7,9], -1, 0, 7))
 
 
