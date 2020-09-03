f6502a40725225f061161aebcf2473592d0393ad
neerajp99 <neerajp1999@gmail.com>
2020-09-03 19:23:35 +0530

    Add bisect method

--- binary_search/first_entry.py
+++ binary_search/first_entry.py
@@ -24,4 +24,23 @@ def first_entry(arr, target):
 			high = mid - 1
 	return None
 
-print(first_entry([-14, -10, 2, 108, 108, 243, 285, 285, 285, 401], 108))
+# print(first_entry([-14, -10, 2, 108, 108, 243, 285, 285, 285, 401], 108))
+
+# Using in-build bisect() in Python
+import bisect 
+
+arr = [-14, -10, 2, 108, 108, 243, 285, 285, 285, 401]
+# Returns the position of the first target element in the array from the left side 
+print(bisect.bisect_left(arr, 108))
+
+# Returns the next posiiton of the target element (last element among in multiple cases) 
+print(bisect.bisect_right(arr, 108))
+
+# Works in the same way as bisect_right 
+print(bisect.bisect(arr,  108))
+
+# insort_left() and insort_right() adds an element to a sorted list
+# such that the new list is also sorted 
+bisect.insort_left(arr, 108)
+bisect.insort_right(arr, 248)
+print(arr)
