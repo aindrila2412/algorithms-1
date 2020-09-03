2a56b120a14ef6d8d9e1db9c2649ae7f3e000d24
neerajp99 <neerajp1999@gmail.com>
2020-09-03 22:46:41 +0530

    Add interger square root method

---
+++ binary_search/integer_sq_root.py
@@ -0,0 +1,16 @@
+"""
+Write a  function that takes a non-negative integer, k, 
+and returns the largest integer whose square is less than or equal to the specified integer k.
+"""
+def int_sq_root(k):
+	low = 0
+	high = k
+
+	while low <= high:
+		mid = (low + high) // 2
+		mid_squared = mid * mid
+		if mid_squared > k:
+			high = mid - 1
+		else:
+			low = mid + 1
+	return low - 1
