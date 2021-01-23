2ecb655150b43c44f7e061d3d048f52a3bc989b0
neerajp99 <neerajp1999@gmail.com>
2021-01-23 13:53:49 +0530

    Add sliding wondow

---
+++ problems/leetcode/lt-209.py
@@ -0,0 +1,24 @@
+# 209. Minimum Size Subarray Sum
+"""
+Given an array of n positive integers and a positive integer s, 
+find the minimal length of a contiguous subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+"""
+class Solution:
+    def minSubArrayLen(self, s: int, nums: List[int]) -> int:
+        min_length = float('inf')
+        left = 0 
+        temp_sum = 0
+        
+        # Loop over for sliding window 
+        for i in range(len(nums)):
+            temp_sum += nums[i]
+            # Once the sum is greater than the given sum, start decrementing the value from the left 
+            while temp_sum >= s:
+                min_length = min(min_length, i + 1 - left)
+                temp_sum -= nums[left]
+                left += 1
+            
+        if min_length != float('inf'):
+            return min_length
+        else:
+            return 0
