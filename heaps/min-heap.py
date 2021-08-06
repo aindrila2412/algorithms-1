8c74961013dddb9ce08b6857d69b15702ad40c6b
neerajp99 <neerajp1999@gmail.com>
2021-08-06 13:24:05 +0530

    Add methods to min heap

--- heaps/min-heap.py
+++ heaps/min-heap.py
@@ -21,13 +21,13 @@ class MinHeap:
         return (2 * index) + 2
 
     def check_for_parent_presence(self, index: int) -> bool:
-        return self.get_left_child_index(index) >= 0
+        return self.get_parent_index(index) >= 0
     
     def check_for_left_child_presence(self, index: int) -> bool:
-        return self.get_left_child_index < self.size 
+        return self.get_left_child_index(index) < self.size 
     
     def check_for_right_child_presence(self, index: int) -> bool:
-        return self.get_right_child_index < self.size
+        return self.get_right_child_index(index) < self.size
 
     def get_parent_value(self, index: int):
         return self.storage[self.get_parent_index(index)]
@@ -60,6 +60,36 @@ class MinHeap:
             self.swap_indexes(self.get_parent_index(index), index)
             self.heapifyUp_recursive(self.get_parent_index(index))
 
+    def heapifyDown_iterative(self):
+        index = 0
+        # Check until there is a left child because then only there will be a right child and a complete tree
+        while self.check_for_left_child_presence(index):
+            # Find the smallest between the two children
+            # Initialise it with the left child index
+            smaller_child_index = self.get_left_child_index(index)
+            # If there is a right child, and the value of the right child is smaller than the left child
+            if (self.check_for_right_child_presence(index) and self.get_right_child_value(index) < self.get_left_child_value(index)):
+                smaller_child_index = self.get_right_child_index(index)
+
+            # Come out of the loop when the parent is smaller than both child
+            if (self.storage[index]) < self.storage[smaller_child_index]:
+                break
+            # Otherwise, swap the parent and the child
+            else:
+                self.swap_indexes(index, smaller_child_index)
+            index = smaller_child_index
+    
+    def heapifyDown_recursive(self, index):
+        smaller_child_index = index 
+        if self.check_for_left_child_presence(index) and self.get_left_child_index(index) < self.storage[index]:
+            smaller_child_index = self.get_left_child_index(index)
+        if self.check_for_right_child_presence(index) and self.get_right_child_index(index) < self.storage[index]:
+            smaller_child_index = self.get_right_child_index(index)
+        if smaller_child_index != index:
+            self.swap_indexes(index, smaller_child_index)
+            self.heapifyDown_recursive(smaller_child_index)
+
+
     def insert_iterative(self, data):
         if self.is_full():
             raise ('Heap is already full')
@@ -77,17 +107,30 @@ class MinHeap:
             self.size += 1
             self.heapifyUp_recursive(self.size - 1)
 
-    def remove_min(self):
+    def remove_min_iterative(self):
         # Handle edge case
-        if len(self.size) == 0:
+        if self.size == 0:
             raise ('Heap is empty!')
         else:
             data = self.storage[0]
+            self.storage[0] = self.storage[self.size - 1]
+            self.size -= 1
+            self.heapifyDown_iterative()
+            return data
 
     
+    def remove_min_recursive(self):
+        if self.size == 0:
+            raise ('Heap is empty!')
+        data = self.storage[0]
+        self.storage[0] = self.storage[self.size - 1]
+        self.size -= 1
+        self.heapifyDown_recursive(0)
+        return data
+
+
     def print_heap(self):
         print(self.storage)    
-
 
 
 x = MinHeap(10)
@@ -98,4 +141,12 @@ x.insert_recursive(5)
 x.insert_recursive(2)
 x.insert_recursive(4)
 x.insert_recursive(0)
+x.print_heap()
+print(x.size)
+x.remove_min_iterative()
+x.print_heap()
+print(x.size)
+x.insert_iterative(0)
+x.print_heap()
+x.remove_min_recursive()
 x.print_heap()
