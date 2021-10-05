3e3658e72bf9e97e22001b9af23e9c06f536c3a3
neerajp99 <neerajp1999@gmail.com>
2021-10-05 22:58:13 +0530

    Add permutation strigng

---
+++ recursion/permutation_strings.py
@@ -0,0 +1,11 @@
+# Creating all the permutation combinations of a given string
+def gen_permutations(values, left, right = ""):
+    if len(left) == 0:
+        values.append(right)
+    else:
+        for i in range(len(left)):
+            gen_permutations(values, left[:i] + left[i+1:], right + left[i])
+
+values = []
+gen_permutations(values, 'abcdefghijk')
+print(values)
